ad9d7beae8f536df80763b3497cb07da128f5e6a
Juancito <juan.fontana.quaresma@gmail.com>
2025-10-25 22:26:39 -0300

    FeaturesEngineering-Completo

--- TRABAJO CREATIVO/features.py
+++ TRABAJO CREATIVO/features.py
@@ -13,22 +13,41 @@ X = data_stopwords[['news_headline']]
 y = data_stopwords[['reliable']]
 X_trn, X_tst, y_trn, y_tst = train_test_split(X, y, test_size=0.2, shuffle=True)
 #print(X_trn)
-
+matriz_embedding = pd.read_csv('TRABAJO CREATIVO/embedding_matrixK.csv', header=None)
+matriz_embedding = matriz_embedding.to_numpy()
 
 #Tokenization and Padding
 max_len = max([len(str(text).split()) for text in X['news_headline']])
 tokenizer = Tokenizer(num_words=max_len,oov_token='<OOV>')
 tokenizer.fit_on_texts(X['news_headline'].tolist())
-
 X_trn_sequences=tokenizer.texts_to_sequences(X_trn['news_headline'].tolist())
 X_trn_padded=pad_sequences(X_trn_sequences,padding='post')
-#print(X_trn_padded)
+
 X_tst_sequences=tokenizer.texts_to_sequences(X_tst['news_headline'].tolist())
 X_tst_padded=pad_sequences(X_tst_sequences,padding='post')
 
-print(X_trn_padded)
-print("##########################")
-print(X_tst_padded)
+#X_trn_padded_v = X_trn_padded.flatten()
+embedding_dim = 100
+seq_len = len(X_trn_padded[0])
+
+
+mat = np.zeros((len(X_trn_padded), seq_len * embedding_dim))
+i = 0
+for x in X_trn_padded:
+   #v = np.zeros((0, 0))
+    vecs = []
+    for token in x:
+        vecs.append(matriz_embedding[token])
+        #v_e = matriz_embedding[token]
+        #v = np.concatenate(v, v_e)
+    v = np.concatenate(vecs)
+    mat[i] = v
+    i+=1
+np.savetxt(r'TRABAJO CREATIVO\TRAINING_DATA_NUMBER.csv', mat, delimiter=',')
+    
+#print(X_trn_padded)
+#print("##########################")
+#print(X_tst_padded)
 
 y_trn_arr = y_trn.values
 y_tst_arr = y_tst.values
@@ -37,3 +56,5 @@ y_tst_arr = y_tst.values
 #Embedding Matrix Initialization
 
 #Integration with Tokenizer
+
+
