569184269ba9edc9dbd57b0523ffc8d64fac55c2
Valentin <fmstylebeats@gmail.com>
2025-11-10 14:36:17 -0300

    Tokenizer Transformer

--- TRABAJO CREATIVO/features.py
+++ TRABAJO CREATIVO/features.py
@@ -8,10 +8,20 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+from transformers import AutoTokenizer
 
 #Ingeniera de caracteristicas
+
+def get_max_len(X):
+    max_len = 0
+    for x in X['news_headline']:
+        if len(str(x).split()) > max_len:
+            max_len = len(str(x).split())
+    return max_len
+
+
 class Features:
-    def __init__(self, data, embedding_matrix):
+    def __init__(self, data, embedding_matrix, tokenizer="keras"):
         data_stopwords = data
         X = data_stopwords[['news_headline']]
         y = data_stopwords[['reliable']]
@@ -30,21 +40,72 @@ class Features:
         # Guardamos la matriz de embeddings
         self.matriz_embedding = embedding_matrix.to_numpy()
 
+        self.max_len = get_max_len(X)
+        print("Max Len: ", self.max_len)
+
         # Tokenización y padding
-        self.max_len = max([len(str(text).split()) for text in X['news_headline']])
-        tokenizer = Tokenizer(num_words=self.max_len, oov_token='<OOV>')
-        tokenizer.fit_on_texts(X['news_headline'].tolist())
+        if tokenizer=="keras":
+            #self.max_len = max([len(str(text).split()) for text in X['news_headline']])
+            tokenizer = Tokenizer(num_words=self.max_len, oov_token='<OOV>')
+            tokenizer.fit_on_texts(X['news_headline'].tolist())
 
-        # Entrenamiento
-        X_trn_sequences = tokenizer.texts_to_sequences(X_trn['news_headline'].tolist())
-        self.X_trn_padded = pad_sequences(X_trn_sequences, maxlen=self.max_len, padding='post', truncating='post')
+            # Entrenamiento
+            X_trn_sequences = tokenizer.texts_to_sequences(X_trn['news_headline'].tolist())
+            self.X_trn_padded = pad_sequences(X_trn_sequences, maxlen=self.max_len, padding='post', truncating='post')
 
-        # Validación
-        X_val_sequences = tokenizer.texts_to_sequences(X_val['news_headline'].tolist())
-        self.X_val_padded = pad_sequences(X_val_sequences, maxlen=self.max_len, padding='post', truncating='post')
+            # Validación
+            X_val_sequences = tokenizer.texts_to_sequences(X_val['news_headline'].tolist())
+            self.X_val_padded = pad_sequences(X_val_sequences, maxlen=self.max_len, padding='post', truncating='post')
 
-        # Test
-        X_tst_sequences = tokenizer.texts_to_sequences(X_tst['news_headline'].tolist())
-        self.X_tst_padded = pad_sequences(X_tst_sequences, maxlen=self.max_len, padding='post', truncating='post')
+            # Test
+            X_tst_sequences = tokenizer.texts_to_sequences(X_tst['news_headline'].tolist())
+            self.X_tst_padded = pad_sequences(X_tst_sequences, maxlen=self.max_len, padding='post', truncating='post')
+        if tokenizer=="transformer":
+            #self.max_len = max([len(str(text).split()) for text in X['news_headline']])
+            tokenizer = AutoTokenizer.from_pretrained("tukx/fake-news-classificator")
+            #tokenizer = Tokenizer(num_words=self.max_len, oov_token='<OOV>')
+            #tokenizer.fit_on_texts(X['news_headline'].tolist())
+
+            # Entrenamiento
+            enc_trn = tokenizer(
+                X_trn['news_headline'].tolist(),
+                padding=True,
+                truncation=True,
+                #max_length=self.max_len,
+                return_tensors='pt'  # también puede ser 'pt' o 'tf'
+            )
+            self.X_trn_padded = enc_trn['input_ids']
+
+            # Validación
+            enc_val = tokenizer(
+                X_val['news_headline'].tolist(),
+                padding=True,
+                truncation=True,
+                #max_length=self.max_len,
+                return_tensors='pt'
+            )
+            self.X_val_padded = enc_val['input_ids']
+
+            # Test
+            enc_tst = tokenizer(
+                X_tst['news_headline'].tolist(),
+                padding=True,
+                truncation=True,
+                #max_length=self.max_len,
+                return_tensors='pt'
+            )
+            self.X_tst_padded = enc_tst['input_ids']
+
+            # Entrenamiento
+            #X_trn_sequences = tokenizer.texts_to_sequences(X_trn['news_headline'].tolist())
+            #self.X_trn_padded = pad_sequences(X_trn_sequences, maxlen=self.max_len, padding='post', truncating='post')
+#
+            ## Validación
+            #X_val_sequences = tokenizer.texts_to_sequences(X_val['news_headline'].tolist())
+            #self.X_val_padded = pad_sequences(X_val_sequences, maxlen=self.max_len, padding='post', truncating='post')
+#
+            ## Test
+            #X_tst_sequences = tokenizer.texts_to_sequences(X_tst['news_headline'].tolist())
+            #self.X_tst_padded = pad_sequences(X_tst_sequences, maxlen=self.max_len, padding='post', truncating='post')
 
 
--- TRABAJO CREATIVO/main.py
+++ TRABAJO CREATIVO/main.py
@@ -20,6 +20,8 @@ dataset_path = 'TRABAJO CREATIVO/SherLockFakenewsNetOriginal.csv'
 dataset_preprocessed_path = 'TRABAJO CREATIVO/SherLockFakenewsProcessedWithStopWords.csv'
 embedding_matrix_path = 'TRABAJO CREATIVO/embedding_matrixKWithStopwords'
 
+tokenizer = "transformer"
+
 
 #---------------------------------------------------CNN---------------------------------------------------#
 cnn_path = 'TRABAJO CREATIVO/cnn.pytorch'
@@ -29,14 +31,14 @@ if not os.path.exists(dataset_preprocessed_path):
     preprocess_data(dataset_path, dataset_preprocessed_path)
 
 if not os.path.exists(embedding_matrix_path):
-    make_embedding_matrix(glove_path, dataset_preprocessed_path, embedding_matrix_path)
+    make_embedding_matrix(glove_path, dataset_preprocessed_path, embedding_matrix_path, tokenizer)
 
 # Finalmente cargamos el dataset
 dataset = pd.read_csv(dataset_preprocessed_path)
 embedding_matrix = pd.read_csv(embedding_matrix_path)
 print("Dataset y matriz de embeddings cargadas correctamente.")
 
-features = Features(dataset, embedding_matrix)
+features = Features(dataset, embedding_matrix, tokenizer)
 
 # Si el modelo no existe, lo entrenamos
 if not os.path.exists(cnn_path):
@@ -48,49 +50,45 @@ test(features, CNN, cnn_path)
 
 #---------------------------------------------------TRANSFORMER---------------------------------------------------#
 # Cargando el transformer
-# model = AutoModelForSequenceClassification.from_pretrained("tukx/fake-news-classificator")
-# ds = load_dataset("tukx/processed_fake_news")
-# tokenizer = AutoTokenizer.from_pretrained("tukx/fake-news-classificator")
-# def preprocess_function(examples):
-#     return tokenizer(examples["text"], truncation=True) # El truncar funciona porque está dentro del tokenizer
-
-# tokenized_ds = ds.map(preprocess_function, batched=True)
-# data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-# accuracy = evaluate.load("accuracy")
-# def compute_metrics(eval_pred):
-#     predictions, labels = eval_pred
-#     predictions = np.argmax(predictions, axis=1)
-#     return accuracy.compute(predictions=predictions, references=labels)
-
-# training_args = TrainingArguments(
-#     output_dir=r"TRABAJO CREATIVO\transformer\Modelo",
-#     learning_rate=2e-5,
-#     per_device_train_batch_size=16,
-#     per_device_eval_batch_size=16,
-#     num_train_epochs=2,
-#     weight_decay=0.01,
-#     eval_strategy="epoch",
-#     save_strategy="epoch",
-#     load_best_model_at_end=True,
-#     push_to_hub=False,
-# )
-
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=tokenized_ds["train"],
-#     eval_dataset=tokenized_ds["test"],
-#     processing_class=tokenizer,
-#     data_collator=data_collator,
-#     compute_metrics=compute_metrics,
-# )
-
-# print("Resultados de Transformer: ")
-
-# results = trainer.evaluate()
-# print(results)
-
+model = AutoModelForSequenceClassification.from_pretrained("tukx/fake-news-classificator")
+ds = load_dataset("tukx/processed_fake_news")
+tokenizer = AutoTokenizer.from_pretrained("tukx/fake-news-classificator")
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True) # El truncar funciona porque está dentro del tokenizer
+tokenized_ds = ds.map(preprocess_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+accuracy = evaluate.load("accuracy")
+f1 = evaluate.load("f1")
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    metrics = accuracy.compute(predictions=predictions, references=labels)
+    metrics.update(f1.compute(predictions=predictions, references=labels))
+    return metrics
+training_args = TrainingArguments(
+    output_dir=r"TRABAJO CREATIVO\transformer\Modelo",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=False,
+)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_ds["train"],
+    eval_dataset=tokenized_ds["test"],
+    processing_class=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+print("Resultados de Transformer: ")
+results = trainer.evaluate()
+print(results)
 
 
 
